6e69d244ad4f53a725a9537bfeb8dfdaff5d5106
Thabang Rakgoropo <rakgoropothabang@gmail.com>
2023-11-10 07:20:14 +0200

    Added tailwind and daisy UI as

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+import ExpanseList from './components/ExpanseList'
+
+function App() {
+ 
+
+  return (
+    <>
+       <ExpanseList/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/ExpanseList.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const ExpanseList = () => {
+  return (
+    <div>
+        <div className="overflow-x-auto">
+  <table className="table">
+    {/* head */}
+    <thead>
+      <tr>
+        <th></th>
+        <th>Description</th>
+        <th>Amount</th>
+        <th>Category</th>
+      </tr>
+    </thead>
+    <tbody>
+      {/* row 1 */}
+      <tr>
+        <th>1</th>
+        <td>Cy Ganderton</td>
+        <td>Quality Control Specialist</td>
+        <td>Blue</td>
+        <td>
+        <button className="btn btn-error">Delete</button>
+        </td>
+        
+      </tr>
+     
+    
+    </tbody>
+  </table>
+</div>
+
+
+
+    </div>
+  )
+}
+
+export default ExpanseList
