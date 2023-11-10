4ead887ee6d1df915144249e4dc24b4f59cbc2ef
Thabang Rakgoropo <rakgoropothabang@gmail.com>
2023-11-10 08:11:56 +0200

    Added the filter and reduce method

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,15 @@ import './App.css'
 import ExpanseList from './components/ExpanseList'
 
 function App() {
- 
+ const [expenses,setExpanses] = useState ([
+  {id:1,description:'Fridge',amount:7000,category:"Utilities"},
+  {id:2,description:'SmartPhone',amount:2000,category:"Tech"},
+  {id:3,description:'Shoes',amount:3000,category:"Clothing"}
+ ])
 
   return (
     <>
-       <ExpanseList/>
+       <ExpanseList expanses={expenses} onDelete={(id)=>{setExpanses(expenses.filter(e=> e.id !=id))}}/>
     </>
   )
 }
--- src/components/ExpanseList.tsx
+++ src/components/ExpanseList.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import  Expanse  from './Expanse';
+import  {Expanse}  from './Expanse';
 
-const ExpanseList = () => {
-    interface Props{
-        expanse:string[]
-    }
+interface Props{
+    expanses:Expanse[];
+    onDelete:(id:number) => void;
+}
+const ExpanseList = ({expanses,onDelete}:Props) => {
+   
   return (
     <div>
         <div className="overflow-x-auto">
@@ -12,27 +13,43 @@ const ExpanseList = () => {
     {/* head */}
     <thead>
       <tr>
-        <th></th>
+       
         <th>Description</th>
         <th>Amount</th>
         <th>Category</th>
       </tr>
     </thead>
     <tbody>
-      {/* row 1 */}
-      <tr>
-        <th>1</th>
-        <td>Cy Ganderton</td>
-        <td>Quality Control Specialist</td>
-        <td>Blue</td>
-        <td>
-        <button className="btn btn-error">Delete</button>
-        </td>
+      {/* Rendering the array*/}
+      
+
+        {expanses.map( expanse => <tr key={expanse.id}>
+            <td>{expanse.description}</td>
+            <td>{expanse.amount}</td>
+            <td>{expanse.category}</td>
+              <td>
+              <button className="btn btn-error" onClick={()=>onDelete(expanse.id)}>Delete</button>
+                </td>  
+           
+        </tr>) }
+       
+      
+       
+       
+      
         
-      </tr>
+     
      
     
     </tbody>
+    <tfoot>
+            <td>Total</td>
+            <td>R{expanses.reduce((total,expense)=> expense.amount+total,0).toFixed(2)}</td>
+            <td>Total</td>
+            <td>Total</td>
+            <td>Total</td>
+
+    </tfoot>
   </table>
 </div>
 
