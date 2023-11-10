98b584223ee6e1632b0331f3af2b0778ec2f3bb5
Thabang Rakgoropo <rakgoropothabang@gmail.com>
2023-11-10 11:30:19 +0200

    Added the @hookform/resolvers

--- src/components/ExpanseForm.tsx
+++ src/components/ExpanseForm.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { categories } from './Expanse'
 import {z} from 'zod';
+import {zodResolver} from '@hook-form/resolvers/zod';
 import {useForm} from 'react-hook-form'
 const schema = z.object({
     description:z.string().min(3).max(50),
@@ -11,17 +12,43 @@ const schema = z.object({
 type ExpanseFormData = z.infer<typeof schema>
 
 const ExpanseForm = () => {
+   const {register,handleSubmit,formState:{errors}} = useForm<ExpanseFormData>({resolver: zodResolver(schema)})
   return (
     <form>
-        <input id="description" type="text" placeholder="Description" className="form-control input input-bordered w-full max-w-xs p-2 m-2" />
-        <select id="category" className="select select-bordered w-full max-w-xs p-2 m-2" >
+        
+        <input {...register('description')} id="description" type="text" placeholder="Description" className="form-control input input-bordered w-full max-w-xs p-2 m-2" />
+        {
+            errors.description && <div className="alert alert-error">
+            <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+            <span>Error! Task failed successfully.</span>
+          </div>
+
+        }
+
+
+
+        <select {...register('category')} id="category" className="select select-bordered w-full max-w-xs p-2 m-2" >
   <option value="" >All Category</option>
 {   categories.map(category=>
      <option key={category} value={category}>{category}</option>
     )}
  
 </select>
-        <input id="amount" type="Number" placeholder="Amount" className="form-control input input-bordered w-full max-w-xs p-2 m-2" />
+{
+            errors.category && <div className="alert alert-error">
+            <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+            <span>Error! Task failed successfully.</span>
+          </div>
+
+        }
+        <input {...register('amount')} id="amount" type="Number" placeholder="Amount" className="form-control input input-bordered w-full max-w-xs p-2 m-2" />
+        {
+            errors.amount && <div className="alert alert-error">
+            <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+            <span>Error! Task failed successfully.</span>
+          </div>
+
+        }
         <button className="btn btn-outline btn-primary w-full max-w-xs p-2 m-2">Submiut</button>
     </form>
   )
